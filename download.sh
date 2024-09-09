394c6f8ca7feb353261e7efc326243160281a792
bortnik1 <qqqqqbortnik@proton.me>
2024-09-09 14:26:18 +0800

    Create download.sh

---
+++ download.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+curl https://download.fosshub.com/Protected/expiretime=1725912532;badurl=aHR0cHM6Ly93d3cuZm9zc2h1Yi5jb20vQnVsay1DcmFwLVVuaW5zdGFsbGVyLmh0bWw=/b0abcaf201640aba29cd0bebf785ca73755231f7200d225d346492cf03abce74/5b964d315dc7e865ea596350/66cfa0f5eeeeed04938b3178/BCUninstaller_5.8.1_portable.zip
+curl https://cdn.iobit.com/dl/iobituninstaller.exe
+curl https://geekuninstaller.com/geek.zip
