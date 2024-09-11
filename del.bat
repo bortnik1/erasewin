49c755fa98f4ff9d5a3270954068f0b0372f99a0
bortnik1 <qqqqqbortnik@proton.me>
2024-09-11 08:22:19 +0800

    Update del.bat

--- del.bat
+++ del.bat
@@ -1,3 +1,11 @@
-"C:\BCUninstaller\win-x64\StoreAppHelper.exe" /uninstall "SpotifyAB.SpotifyMusic_1.171.560.0_x86__zpdnekdrzrea0"
-
-
+reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies" /f
+reg delete "HKCU\Software\Microsoft\WindowsSelfHost" /f
+reg delete "HKCU\Software\Policies" /f
+reg delete "HKLM\Software\Microsoft\Policies" /f
+reg delete "HKLM\Software\Microsoft\Windows\CurrentVersion\Policies" /f
+reg delete "HKLM\Software\Microsoft\Windows\CurrentVersion\WindowsStore\WindowsUpdate" /f
+reg delete "HKLM\Software\Microsoft\WindowsSelfHost" /f
+reg delete "HKLM\Software\Policies" /f
+reg delete "HKLM\Software\WOW6432Node\Microsoft\Policies" /f
+reg delete "HKLM\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Policies" /f
+reg delete "HKLM\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\WindowsStore\WindowsUpdate" /f
